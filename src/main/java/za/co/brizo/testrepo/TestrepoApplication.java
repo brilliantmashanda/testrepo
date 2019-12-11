07e48c752c154a1f810abe43afd3151809af4cd7
brilliantmashanda <masebenza.bm@gmail.com>
2019-12-11 21:08:28 +0200

    Commit First

---
+++ src/main/java/za/co/brizo/testrepo/TestrepoApplication.java
@@ -0,0 +1,13 @@
+package za.co.brizo.testrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestrepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestrepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/za/co/brizo/testrepo/TestrepoApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.brizo.testrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestrepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
